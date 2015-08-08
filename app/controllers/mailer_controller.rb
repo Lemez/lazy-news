4423d9cd07f8b0a684fa0c8aa0c0cfb58155d7d7
Jonathan Walton <lemez9@gmail.com>
2015-08-08 22:22:44 +0530

    mailer layout inline improvemenets

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -1,6 +1,6 @@
 class MailerController < ApplicationController
 
-  def preview_welcome()
+  def preview_welcome
     @stories = Story.is_music.last_week.order(:modified).reverse
     @genre = 'music'
     render :file => 'mailer/weekly_email.html.erb', :layout => 'mailer_layout'
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -21,3 +21,4 @@ class UserMailer < ActionMailer::Base
 		end
 	end
 end
+
