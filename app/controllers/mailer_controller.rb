d245c0f2487379f28a2d5ed33f4a98fba9c8c8f4
Jonathan Walton <lemez9@gmail.com>
2015-08-10 19:01:24 +0530

    mailer better. NB previous commit, included RSPEC TEST SETUP

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -1,6 +1,7 @@
 class MailerController < ApplicationController
 
   def preview_welcome
+  	@companies = Startup.order(:modified).reverse[0..4]
     @stories = Story.is_music.where(:modified => 1.week.ago..Time.now).order(:modified).order(source: :desc).reverse
     @genre = 'music'
     render :file => 'mailer/weekly_email.html.erb', :layout => 'mailer_layout'
