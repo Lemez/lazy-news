05afa63a4e7b78fa44e921f5f9c1f74d795c9272
Jonathan Walton <lemez9@gmail.com>
2015-08-15 17:27:30 +0530

    UI on mailer better, also deleted click model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ News::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
 end
--- spec/factories/clicks.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :click do
-    story_id 1
-clicks 1
-  end
-
-end
