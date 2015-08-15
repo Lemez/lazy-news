11c31f5d52feceb5cd5154063c3a77cc6e9a95c8
Jonathan Walton <lemez9@gmail.com>
2015-08-15 22:15:10 +0530

    added absolute logo urls for mailer

---
+++ config/initializers/logo_urls.rb
@@ -0,0 +1,10 @@
+ABSOLUTE_LOGO_URLS = {
+'betalist' => "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xtf1/v/t1.0-1/p160x160/10001450_806669462679858_413144091_n.png?oh=7cb69daa917fa1d4262680b798e24e35&oe=567AE588&__gda__=1446986145_ddc1280fa538b258e4bed867472d6020",
+'musically' => 'http://musically.com/wp-content/uploads/2013/09/new-music-ally-logo-300x76.png',
+'wired' => 'https://makerclub.org/app/themes/roots/assets/img/sponsor-logos/600w/wired-logo.png',
+'mit' => 'http://startupfocus.saphana.com/wp-content/uploads/2015/04/mit-tech-review-300x142.jpg',
+'musicbusinessworldwide' => 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSIj9YTCQsW2-DSHdy9gk1sreakygnYjESe8yBn-NC1-0iLmDvhs-TPaKU',
+'cmu' => 'http://jennaleepr.com/wp-content/uploads/2015/05/CMU-Logo1.jpg',
+'rollingstone' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReALB-6YWAAHqU-Y4LkSyQ_BNKHzfn40Eft39BOK_CLH7mger5mA',
+'venturebeat' => 'https://www.defcon.org/images/defcon-main/VB-logo.jpg'
+}
